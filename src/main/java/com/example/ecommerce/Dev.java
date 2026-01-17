fbe0216f5874d863f996fb51ae694e92a8354f1b
yoojanghun <yoojanghun@naver.com>
2026-01-17 12:50:16 +0900

    Dependency Injection
    .run()으로 JVM 내부에 IOC Container 만듦. Spring은 그 Container 내부에 객체를 자동 생성해줌.
    
    우리가 직접 new 키워드로 객체를 만드는 것이 아님.
    
    IOC Container의 data type은 ApplicationContext.
    
    spring에게 어떤 class를 관리(객체 생성)해야 할 지 말해주기 위해 @Component annotation 사용

---
+++ src/main/java/com/example/ecommerce/Dev.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build() {
+        System.out.println("Working on great project!!");
+    }
+}
--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -2,12 +2,21 @@ package com.example.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
-	}
+
+        ApplicationContext context = SpringApplication.run(EcommerceApplication.class, args);
+
+        Dev obj = context.getBean(Dev.class);
+
+        obj.build();
+    }
 
 }
+
+// 내가 직접 new 를 사용하여 객체를 만들지 않음.
+// @Component로 어떤 class를 관리해야할 지 spring에게 말하면 spring이 객체 자동 생성
