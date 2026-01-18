a64455467bd7b13657df597252c192ec23fd47ee
yoojanghun <yoojanghun@naver.com>
2026-01-18 16:54:25 +0900

    CORS 설정함
    우선 localhost에서 테스트할 때는 controller에 @CrossOrigin만 붙이면 됨
    
    실제 개발할 때는 @Configuration 파일 붙여서 특정 url의 client가 요청할 때만 서버의 자원 제공

--- src/main/java/com/example/ecommerce/controller/ProductController.java
+++ src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.ecommerce.controller;
 import com.example.ecommerce.model.Product;
 import com.example.ecommerce.repository.ProductRepository;
 import com.example.ecommerce.service.ProductService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
