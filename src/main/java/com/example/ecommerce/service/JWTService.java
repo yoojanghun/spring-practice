6f4c8b7f45afe7df6c1eeb2a5270a38e893b654a
yoojanghun <yoojanghun@naver.com>
2026-01-21 09:51:47 +0900

    login 시 JWT 토큰 생성

--- src/main/java/com/example/ecommerce/service/JWTService.java
+++ src/main/java/com/example/ecommerce/service/JWTService.java
@@ -1,10 +1,38 @@
 package com.example.ecommerce.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JWTService {
-    public String generateToken() {
-        return "Here is your token";
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expirationMs;
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .addClaims(claims)              // JWT payload(내용부분)에 key-value 데이터 추가
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(getkey())
+                .compact();
+    }
+
+    private Key getkey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);        // String -> byte
+        return Keys.hmacShaKeyFor(keyBytes);                        // byte -> Key(HMAC-SHA256)
     }
 }
--- src/main/java/com/example/ecommerce/service/UserService.java
+++ src/main/java/com/example/ecommerce/service/UserService.java
@@ -34,7 +34,7 @@ public class UserService {
                 .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
 
         if (authentication.isAuthenticated()) {
-            return jwtService.generateToken();
+            return jwtService.generateToken(user.getUsername());
         }
 
         return "Fail";
